ea4c816f8dbb70b0bf11a794efa7aa0d9f6bc780
Carlos Iván <carlosivanhb@hotmail.com>
2025-09-02 13:58:26 -0600

    Nueva clase EasyLoRa_Messenger  para el manejo de mensajes

---
+++ lib/EasyLoRa_Messenger/EasyLoRa_Messenger.hpp
@@ -0,0 +1,13 @@
+#ifndef EASY_LORA_MESSENGER_HEADER
+#define EASY_LORA_MESSENGER_HEADER
+
+class EasyLoRa_Messenger {
+public:
+    EasyLoRa_Messenger();
+
+private:
+
+
+};
+
+#endif // !EASY_LORA_MESSENGER_HEADER
